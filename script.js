0d63a79ffe60299c06263964f6376869a2a527af
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-02-17 15:47:58 +0530

    completed

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
-function loading(){
-    var tl=gsap.timeline();
+function loading() {
+    var tl = gsap.timeline();
 
-tl.to("#yellow1",{
-top:"-100%",
-delay:0.5,
-duration:0.7,
-ease:"expo.out"
-})
-
-
-tl.to("#yellow2",{
-    top:"-100%",
-    delay:0.6,
-    duration:0.7,
-    ease:"expo.out"
-    },"anim")
-
-    tl.to("#loader h1",{
-        delay:0.6,
-        color:"black",
-        duration:"0.7"
-    },"anim")
-
-     
-    tl.to("#loader",{
-        opacity:"0",
+    tl.to("#yellow1", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.7,
+        ease: "expo.out"
     })
-   
-    
-    tl.to("#loader",{
-        display:"none",
+
+
+    tl.to("#yellow2", {
+        top: "-100%",
+        delay: 0.6,
+        duration: 0.7,
+        ease: "expo.out"
+    }, "anim")
+
+    tl.to("#loader h1", {
+        delay: 0.6,
+        color: "black",
+        duration: "0.7"
+    }, "anim")
+
+
+    tl.to("#loader", {
+        opacity: "0",
     })
-    
+
+
+    tl.to("#loader", {
+        display: "none",
+    })
+
 }
 
-loading()
+loading();
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+function loco() {
 
-var elems=document.querySelectorAll(".elem");
-var page2=document.querySelector("#page2");
-elems.forEach(function(ele){
-    ele.addEventListener("mouseenter",function(){
-        var bgimg=ele.getAttribute("data-img")
-        page2.style.backgroundImage=`url(${bgimg})`;
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+
+
+    document.querySelector("#footer h2").addEventListener("click", () => {
+        scroll.scrollTo(0);
     })
-})
+
+
+    var elems = document.querySelectorAll(".elem");
+    var page2 = document.querySelector("#page2");
+    elems.forEach(function (ele) {
+        ele.addEventListener("mouseenter", function () {
+            var bgimg = ele.getAttribute("data-img")
+            page2.style.backgroundImage = `url(${bgimg})`;
+        })
+    })
+}
+
+loco();
